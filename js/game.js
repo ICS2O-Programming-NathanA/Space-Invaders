a4c95caf54c72dbd3d73f1e74fb973dcc2131520
Nathan Araujo <99274283+nathan-araujo@users.noreply.github.com>
2022-06-02 15:11:09 +0000

    added comments

--- js/game.js
+++ js/game.js
@@ -1,40 +1,45 @@
 /* global Phaser */
 
+// Copyright (c) 2022 Nathan Araujo All rights reserved
+//
 // Created by Nathan Araujo
 //Created on June 1 2022
 //Phaser 3 config file
 
+//imports the scenes
 import SplashScene from './splashSceen.js'
 
-// Game scene
+// create the scenes
 const splashScene = new SplashScene()
 
-/*Game scene*/
+/**
+  * Start Phaser Game. 
+  */
 const config = {
-type: Phaser.AUTO,
-width: 1920,
-height: 1080,
-physics: {
-default: 'arcade',
-arcade: {
-debug: true
-}
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+  },
 },
-  //Sets background color
+  //Sets background color to grey
 backgroundColor: 0x5f6e7a,
   scale: {
     mode: Phaser.Scale.FIT,
-    //we place it in the middle of the page
+    //centers the image in the middle of the screen
     autoCenter: Phaser.Scale.CENTER_BOTH
-  }
+  },
 }
 
 const game = new Phaser.Game(config)
-
+//console.log(game)
 
 // load scenes
 // NOTE: remember any "key" is gloabal and CAN NOT be resued
 game.scene.add('splashScene', splashScene)
 
-// start scene
+// the start scene
 game.scene.start('splashScene')
--- js/splashScene.js
+++ js/splashScene.js
@@ -6,23 +6,28 @@
 // Created on: June 1 2022
 // This is the Splash Scene
 
+//This clas is the Splash Scene.
 class SplashScene extends Phaser.Scene {
+  //This method is the constructor
   constructor () {
     super({ key: 'splashScene' })
   }
-
+  //This method is aclled bu the Scene Manager when the scene starts, befroe preload and create
   init (data) {
+    //color is black
     this.cameras.main.setBackgroundColor('#ffffff')
   }
-
+  //Use it to laod assets
   preload () {
-    console.log('Splash Scene')
+    console.log("Splash Scene")
   }
-
+  // Use it to create games objects
   create (data) {
+    //pass
   }
-
+  //This method is aclled once pergame step while the scene is running
   update (time, delta) {
+    //pass
   }
 }
 
