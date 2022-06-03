09add70839678dfee8046940603a3efd784facb4
Nathan Araujo <99274283+nathan-araujo@users.noreply.github.com>
2022-06-03 21:01:54 +0000

    updated game.js

--- js/game.js
+++ js/game.js
@@ -7,7 +7,7 @@
 //Phaser 3 config file
 
 //imports the scenes
-import SplashScene from './splashSceen.js'
+import SplashScene from "./splashScene.js"
 
 // create the scenes
 const splashScene = new SplashScene()
@@ -20,13 +20,13 @@ const config = {
   width: 1920,
   height: 1080,
   physics: {
-    default: "arcade",
+    default: 'arcade',
     arcade: {
-      debug: true,
+      debug: true
   },
 },
   //Sets background color to grey
-backgroundColor: 0x5f6e7a,
+  backgroundColor: 0xFFFFFF,
   scale: {
     mode: Phaser.Scale.FIT,
     //centers the image in the middle of the screen
@@ -35,7 +35,7 @@ backgroundColor: 0x5f6e7a,
 }
 
 const game = new Phaser.Game(config)
-//console.log(game)
+console.log(game)
 
 // load scenes
 // NOTE: remember any "key" is gloabal and CAN NOT be resued
