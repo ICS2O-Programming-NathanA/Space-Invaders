2472a76b6332424fcb3b3c11851796e843e7bd83
Nathan Araujo <99274283+nathan-araujo@users.noreply.github.com>
2022-06-10 01:25:00 +0000

    added score and need to fix destroying player

--- js/gameScene.js
+++ js/gameScene.js
@@ -31,6 +31,12 @@ class GameScene extends Phaser.Scene {
     this.background = null
     this.ship = null
     this.fireMissile = false
+    this.score = 0
+    this.scoreText = null
+    this.scoreTextStyle = { font: '65px Arial', fill: '#ffffff', align: 'center'}
+
+    this.gameOverText = null
+    this.gameOverTextStyle = {font: '65px Arial', fill: '#ff0000', align: 'center'}
   }
   
   //Sets up the base state of the scene
@@ -56,11 +62,19 @@ class GameScene extends Phaser.Scene {
     
     //sound for missile
     this.load.audio('laser', 'assets/shoot.wav')
+
+    //sound for alien being destroyed
+    this.load.audio('explosion', 'assets/barrelExploding.wav')
+
+    //sound for alien being destroyed
+    this.load.audio('boom', 'assets/bomb.wav')
   }
   //displays the content to the user
   create (data) {
     this.background = this.add.image(0, 0, 'spaceinvaders-background').setScale(3.0)
     this.background.setOrigin(0, 0)
+
+    this.scoreText = this.add.text(10, 10, 'Score: ' + this.score.toString(), this.scoreTextStyle)
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
 
@@ -70,6 +84,28 @@ class GameScene extends Phaser.Scene {
     //creates a group for the aliens
     this.alienGroup = this.add.group()
     this.createAlien()
+
+    //Collisons between missiles and aliens
+    this.physics.add.collider(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
+      alienCollide.destroy()
+      missileCollide.destroy()
+      this.sound.play('explosion')
+      this.score = this.score + 1
+      this.scoreText.setText('Score: ' + this.score.toString())
+      this.createAlien()
+      this.createAlien()
+    }.bind(this))
+
+    //Collisons between player and alien
+    this.physics.add.collider(this.ship, this.alienGroup, function (shipCollide, alienCollide) {
+      this.sound.play('bomb')
+      this.physics.pause()
+      alienCollide.destroy()
+      shipCollide.destroy()
+      this.gameOverText = this.add.text(1920 / 2, 'Game Over!\nClick to play again.', this.gameOverTextStyle).setOrigin(0.5)
+      this.gameOverText.setInteractive({ useHandCursor: true })
+      this.gameOverText.on('pointerdown', () => this.scene.start('gameScene'))
+    }.bind(this))
   }
   
   update (time, delta) {
